b18af29ae3df5dd8cb1d44539cb27e3fec93ed72
Keira Hartley <khartley@lcs.on.ca>
2019-10-10 11:09:36 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Hartley, Keira on 2019-10-10.
+//  Copyright © 2019 Hartley, Keira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
