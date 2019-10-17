13752d45a651c2b8bcdbb45c53af4aa4b19e16d3
Keira Hartley <khartley@lcs.on.ca>
2019-10-17 09:20:04 -0400

    Got the code to calculate the tip and tip per person.

---
+++ Tip Calculator/TipCalculatorPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let amountDecimal = 55.00
+let tipDecimal = 15.0
+
+
+var calculateTip = amountDecimal*(tipDecimal/100)
+
+print(calculateTip)
--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -13,12 +13,35 @@ class ViewController: UIViewController {
     //MARK: Properties
     
     
+    @IBOutlet weak var tipPerPerson: UILabel!
+    @IBOutlet weak var tipTotal: UILabel!
+    @IBOutlet weak var buttonCalculate: UIButton!
+    @IBOutlet weak var numberOfPeople: UITextField!
+    @IBOutlet weak var tipPercent: UITextField!
+    @IBOutlet weak var billAmount: UITextField!
     
     //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let amountDecimal = Double(billAmount.text!)!
+        let tipDecimal = Double(tipPercent.text!)!
+        let numberOfPeopleDecimal = Double(numberOfPeople.text!)!
+      
+        let calculateTip = amountDecimal*(tipDecimal/100)
+        
+        let calculatePerPerson = calculateTip/numberOfPeopleDecimal
+        
+        tipTotal.text = 
+        
+    }
+    
+    
+
 
 
 }
